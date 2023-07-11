37aa75e1c678a304086417190e418986110951d2
Mark Goodrich <mgoodrich@pih.org>
2023-07-11 16:34:40 -0400

    update to O3 V5

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,26 @@
-import {
-  defineConfigSchema,
-  Type,
-  getAsyncLifecycle,
-  isUrl,
-} from "@openmrs/esm-framework";
+import { defineConfigSchema, getAsyncLifecycle } from "@openmrs/esm-framework";
 import { configSchema } from "./configSchema";
+
+const moduleName = "@pih/esm-refapp-navbar-app";
+const options = {
+  featureName: "Refapp Navbar",
+  moduleName,
+};
 
 const backendDependencies = { appui: "^1.0.0" };
 
-const importTranslation = require.context(
+export const importTranslation = require.context(
   "../translations",
   false,
   /.json$/,
   "lazy"
 );
 
-function setupOpenMRS() {
+export const root = getAsyncLifecycle(
+  () => import("./root.component"),
+  options
+);
+
+export function startupApp() {
   defineConfigSchema("@pih/esm-refapp-navbar-app", configSchema);
-
-  const moduleName = "@pih/esm-refapp-navbar-app";
-
-  const options = {
-    featureName: "Refapp Navbar",
-    moduleName,
-  };
-
-  return {
-    pages: [
-      {
-        load: getAsyncLifecycle(() => import("./root.component"), options),
-        route: () => true,
-        order: 0,
-      },
-    ],
-  };
 }
-
-export { backendDependencies, importTranslation, setupOpenMRS };
